a9540197c25d05d1187584f6e14b3f8e9f252b33
pranavv481 <pranavv481@gmail.com>
2020-05-24 20:49:03 +0530

    http post

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import PostForm from './components/PostForm';
+
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+        <PostForm />
+      </div>
+    );
+  }
+}
+
+export default App;
